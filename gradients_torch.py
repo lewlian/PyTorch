5e1d3f04ef664113062d75712f61a317c7f6dbe2
Sean <seanlewtengsiong@gmail.com>
2020-12-01 16:32:39 +0800

    changed prediction to PyTorch Model

--- gradients_torch.py
+++ gradients_torch.py
@@ -17,21 +17,30 @@ import torch.nn as nn  # neural network module
 # f = w * x
 # f = 2 * x
 
-X = torch.tensor([1, 2, 3, 4], dtype=torch.float32)
-Y = torch.tensor([2, 4, 6, 8], dtype=torch.float32)
+X = torch.tensor([[1],[2], [3], [4]], dtype=torch.float32)
+Y = torch.tensor([[2], [4], [6], [8]], dtype=torch.float32)
 
-w = torch.tensor(0.0, dtype=torch.float32, requires_grad=True)
+X_test = torch.tensor([5], dtype=torch.float32)
+
+n_samples, n_features = X.shape
+print(n_samples, n_features)
+
+input_size = n_features
+output_size = n_features
+
+model = nn.Linear(input_size,output_size)
+
+# w = torch.tensor(0.0, dtype=torch.float32, requires_grad=True)
 
 # calculate model prediction
-
-
-def forward(x):
-    return w * x
+# def forward(x):
+#     return w * x
 
 # calculate loss = MSE in the case of linear regression
 # def loss(y, y_predicted):
 #     return ((y_predicted-y)**2).mean()
 
+print(f'Prediction before trianing: f(5) = {model(X_test).item():.3f}')
 
 # Training
 learning_rate = 0.01
@@ -39,11 +48,11 @@ n_iters = 100
 
 loss = nn.MSELoss()
 # Stochastic Gradient Descent
-optimizer = torch.optim.SGD([w], lr=learning_rate)
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
 
 for epoch in range(n_iters):
     # prediction = forward pass
-    y_pred = forward(X)
+    y_pred = model(X)
 
     # loss
     l = loss(Y, y_pred)
@@ -60,6 +69,7 @@ for epoch in range(n_iters):
     optimizer.zero_grad()
 
     if epoch % 10 == 0:
-        print(f'epoch {epoch+1}: w = {w:.3f}, loss = {l:.8f}')
+        [w, b] = model.parameters()
+        print(f'epoch {epoch+1}: w = {w[0][0].item():.3f}, loss = {l:.8f}')
 
-print(f'Prediction before trianing: f(5) = {forward(5):.3f}')
+print(f'Prediction after trianing: f(5) = {model(X_test).item():.3f}')
