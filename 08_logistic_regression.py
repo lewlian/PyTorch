56c1a24975e3d9e7fcdcac7b437d5c91df5c45f4
Sean <seanlewtengsiong@gmail.com>
2020-12-23 17:02:22 +0800

    dataloader

--- 08_logistic_regression.py
+++ 08_logistic_regression.py
@@ -19,8 +19,7 @@ n_samples, n_features = X.shape
 
 X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2, random_state=1234)
 
-# scale
-# Common requirement for machine learning: can behave bad if they are not standard normally distributed
+#scale
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.fit_transform(X_test)
